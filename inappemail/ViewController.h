d9ddc2034575df92a30ab9cad4c74f7ab40226fb
Zorigt <Zorigt@gmail.com>
2014-06-21 21:55:08 -0700

    Initial Commit

---
+++ inappemail/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  inappemail
+//
+//  Created by Baz on 6/21/14.
+//  Copyright (c) 2014 Deal Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ inappemail/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  inappemail
+//
+//  Created by Baz on 6/21/14.
+//  Copyright (c) 2014 Deal Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
