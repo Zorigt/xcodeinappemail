595956b66dc285087b86202aec96fadd4a423e17
Zorigt <Zorigt@gmail.com>
2014-06-21 22:21:29 -0700

    in app email

--- inappemail/ViewController.m
+++ inappemail/ViewController.m
@@ -26,4 +26,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sendEmail:(id)sender {
+    MFMailComposeViewController * composer = [[MFMailComposeViewController alloc] init];
+    [composer setMailComposeDelegate:self];
+    if ([MFMailComposeViewController canSendMail]) {
+        [composer setToRecipients:[NSArray arrayWithObjects:@"email@email.com", nil]];
+        [composer setSubject:@"New Awsome App"];
+        [composer setMessageBody:@"Hello, \nIt's an app that sends email." isHTML:NO];
+        [composer setModalTransitionStyle:UIModalTransitionStyleCrossDissolve];
+        [self presentModalViewController:composer animated:YES];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Erronous" message:@"E has occurred" delegate:nil cancelButtonTitle:@"Let's do this" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+-(void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Errata" message:@"It has occuren!" delegate:nil cancelButtonTitle:@"Okey" otherButtonTitles:nil, nil];
+        [alert show];
+        [self dismissModalViewControllerAnimated:YES];
+    }
+}
 @end
